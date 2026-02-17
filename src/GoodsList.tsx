4cf1842cb942e4cc33ad2962189d29b3eadedd7e
Hanna Yatskiv <hanna.ada7@gmail.com>
2026-02-17 10:32:31 +0300

    add task solution

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,12 +5,18 @@ type Props = {
   goods: Good[];
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+export const GoodsList: React.FC<Props> = React.memo<Props>(({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={good.id} data-cy="good">
+      <li
+        key={good.id}
+        data-cy="good"
+        style={{ color: good.color ?? 'inherit' }}
+      >
         {good.name}
       </li>
     ))}
   </ul>
-);
+));
+
+GoodsList.displayName = 'GoodsList';
